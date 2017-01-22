c303e9ccec53477d595b2147d28c134e17d5bf86
Marko Harjula <marko.harjula@gmail.com>
2017-01-22 10:59:56 +0200

    Update main.js

--- Endpoint/main.js
+++ Endpoint/main.js
@@ -7,7 +7,11 @@ nconf.argv()
    .env()
    .file({ file: __dirname+'/config.json' });
  
+// Device to Cloud
 var EventHubClient = require('azure-event-hubs').Client;
+// Cloud to Device
+var Client = require('azure-iothub').Client;
+var Message = require('azure-iot-common').Message;
 var last_message = null; // global
 
 // app.js
@@ -42,8 +46,35 @@ wsServer.on('request', function(request) {
     });
 });
  
-
-// Azure IoT
+// Cloud to Device client
+var targetDevice = 'myFirstDevice';
+var serviceClient = Client.fromConnectionString( nconf.get("azure_iot_connection") );
+ serviceClient.open(function (err) {
+   if (err) {
+     console.error('Could not connect: ' + err.message);
+   } else {
+     console.log('Service client connected');
+     serviceClient.getFeedbackReceiver(receiveFeedback);
+     var message = new Message('Cloud to device message.');
+     message.ack = 'full';
+     message.messageId = "My Message ID";
+     console.log('Sending message: ' + message.getData());
+     serviceClient.send(targetDevice, message, printResultFor('send'));
+   }
+ });
+ function receiveFeedback(err, receiver){
+   receiver.on('message', function (msg) {
+     console.log('Feedback message:');
+     console.log(msg.getData().toString('utf-8'));
+   });
+ }
+ function printResultFor(op) {
+   return function printResult(err, res) {
+     if (err) console.log(op + ' error: ' + err.toString());
+     if (res) console.log(op + ' status: ' + res.constructor.name);
+   };
+ }
+// Device to Cloud client
 var client = EventHubClient.fromConnectionString( nconf.get("azure_iot_connection") );
 client.open()
 	.then(client.getPartitionIds.bind(client))
@@ -57,7 +88,9 @@ client.open()
 		});
 	})
 	.catch(printError);
-	 
+
+
+
 var printError = function (err) {
 	console.log(err.message);
 };
@@ -66,3 +99,13 @@ var printMessage = function (message) {
 	last_message = message.body;
 	wsServer.broadcastUTF(JSON.stringify(message.body));
 };
+
+//
+app.get('/hello', function(req, res){
+     var message = new Message(JSON.stringify({"type":"hello", "value":req.query.value}));
+     message.ack = 'full';
+     message.messageId = "My Message ID";
+     console.log('Sending message: ' + message.getData());
+     serviceClient.send(targetDevice, message, printResultFor('send'));
+	 res.send('{"status":"sent"}');
+});
